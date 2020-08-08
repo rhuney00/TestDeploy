644bc17a39b433b96c2e86c45f96c9039daae471
rhuney00 <rhuney00@gmail.com>
2020-08-08 14:32:43 -0500

    Update test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Testing</h1>
+      <p>Another test</p>
     </div>
   );
 }
